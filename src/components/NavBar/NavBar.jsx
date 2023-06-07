40d5b05e7bbc2ef0e25734d32a625c929433da51
Daniel <derrysantaclos@gmail.com>
2023-06-07 14:18:11 +0100

    initiate search styling and clear App.css styles

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,12 +5,14 @@ const NavBar = () => {
   return (
     <>
     <div className="nav-container">
-      <ul className="nav-item nav-item-group-1 nav-list">
+      <ul className="nav-item nav-item-group-1 nav-list fw-400 fs-14">
         <li className="pointer-cursor nav-hov-black">BUY</li>
         <li className="pointer-cursor nav-hov-black">SELL</li>
-        <li className="pointer-cursor nav-hov-black">Deals</li>
+        <li className="pointer-cursor nav-hov-black">DEALS</li>
       </ul>
-      <div className="nav-item nav-item-group-2 nav-search">Search</div>
+      <div className="nav-item nav-item-group-2 nav-search">
+        <input type="text" className='nav-search-input' placeholder='Search' />
+      </div>
       <ul className="nav-item nav-list nav-item-group-3">
         <li className= "user-profile pointer-cursor">Sign in</li>
         <li className="pointer-cursor">Cart</li>
