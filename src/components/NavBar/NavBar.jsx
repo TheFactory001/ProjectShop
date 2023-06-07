884359d7d01b50aca3e7080fd8025652b26c1a21
Daniel <derrysantaclos@gmail.com>
2023-06-07 12:39:30 +0100

    add hover effects

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,14 +6,14 @@ const NavBar = () => {
     <>
     <div className="nav-container">
       <ul className="nav-item nav-item-group-1 nav-list">
-        <li>BUY</li>
-        <li>SELL</li>
-        <li>Deals</li>
+        <li className="pointer-cursor nav-hov-black">BUY</li>
+        <li className="pointer-cursor nav-hov-black">SELL</li>
+        <li className="pointer-cursor nav-hov-black">Deals</li>
       </ul>
       <div className="nav-item nav-item-group-2 nav-search">Search</div>
       <ul className="nav-item nav-list nav-item-group-3">
-        <li className= "user-profile">Sign in</li>
-        <li>Cart</li>
+        <li className= "user-profile pointer-cursor">Sign in</li>
+        <li className="pointer-cursor">Cart</li>
       </ul>
     </div>
     
