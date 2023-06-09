b5703520d61cd5ddd5c71fb539ab1558c23161c1
Daniel <derrysantaclos@gmail.com>
2023-06-09 11:42:17 +0100

    fix rating

--- src/components/MarketBody/MarketBody.jsx
+++ src/components/MarketBody/MarketBody.jsx
@@ -4,16 +4,17 @@ import SellerCard from '../SellerCard/SellerCard'
 
 const MarketBody = () => {
     //demo seller details - should be passed from the main component
-    const demo_seller_details ={distant:5 ,user: {name:"Daniel", title:"Eunice's Clothing", customer_count:50}}
+    const demo_seller_details_1 ={distant:5 ,seller: {name:"Betha", title:"Betha's Hair",services:["Hairstyling", "Wigs", "Make-Up"], customer_count:50, rating:4.5}}
+    const demo_seller_details_2 ={distant:15 ,seller: {name:"Eunice", title:"Eunice's Clothing",services:["Baby Wears", "Formal", "Casual"], customer_count:200, rating:3.5}}
   return (
     <div className='pl-pr-50 mt-25 '>
         <div className='market-body p-25 '>
             <div className='body-text opac-50 fs-14'><p>Top shops around you</p></div>
             <div className="flex">
 
-            <SellerCard distant = "5km" seller_details={demo_seller_details} />
-            <SellerCard distant = "5km" seller_details={demo_seller_details} />
-            <SellerCard distant = "5km" seller_details={demo_seller_details} />
+            <SellerCard distant = "5km" seller_details={demo_seller_details_1} />
+            <SellerCard distant = "5km" seller_details={demo_seller_details_2} />
+            <SellerCard distant = "5km" seller_details={demo_seller_details_2} />
             </div>
             <div className="flex-center"><button className='enter-market-btn pointer-cursor' >ENTER THE MARKET</button></div>
             
--- src/components/SellerCard/SellerCard.jsx
+++ src/components/SellerCard/SellerCard.jsx
@@ -3,34 +3,48 @@ import './SellerCard.css'
 import tempo from '../../asset/Ibadan.jpg'
 
 const SellerCard = ({seller_details}) => {
-    const seller_services = ["Hairstyling", "Wigs", "Make-Up"]
+    const rate = seller_details.seller.rating;
+    const unrate = 5-rate
     
   return (
     <div className='m-25' >
         <div className='seller-distance fs-16'><p>{seller_details.distant}km</p></div>
         <div className='seller-card-profile'>
-            <div className="shop-title flex-8 fw-700"><p>{seller_details.user.title}</p></div>
+            <div className="shop-title flex-8 fw-700"><p>{seller_details.seller.title}</p></div>
             <div className="img-customer">
             <img src={tempo} alt="" className='seller-img'/>
                 
                 <div className="customer">
-                    <p htmlFor=""><span>Customer:</span> <span>{seller_details.user.customer_count}</span></p>
+                    <p htmlFor=""><span>Customer:</span> <span>{seller_details.seller.customer_count}</span></p>
                 </div>
             </div>
             <div className="rating flex-8">
                 <p >Rating</p>
+                {/* write logic to ensure the rating gotten as number is well interpreted */}
+              
+               
                 <div className="rate-icons">
-                    <i className="fa-solid fa-star"></i>
-                    <i className="fa-solid fa-star"></i>
-                    <i className="fa-solid fa-star"></i>
-                    <i className="fa-solid fa-star-half-stroke"></i>
-                    <i className="fa-solid fa-star-half-stroke"></i>
+                    {/*full star ratings for whole numbers*/}
+                    {[...Array(Math.floor(rate))].map((i,e)=>
+                         <i className="fa-solid fa-star" key={e}></i> 
+                    )}
+                    {
+                        //half star rating for fractions 
+                     (rate%1)!==0? 
+                     <i className="fa-solid fa-star-half-stroke"></i>:null
+                    }
+
+                     {
+                        // no fill left portion
+                     [...Array(Math.floor(unrate))].map((i,e)=>
+                         <i className="fa-regular fa-star" key={e}></i> 
+                    )}
                 </div>
             </div>
             <div className="services">
                 <ul className='flex-btw'>
                     
-                    {seller_services.map((service)=>
+                    {seller_details.seller.services.map((service)=>
                         <li>{service}</li>
                     )}
                   
