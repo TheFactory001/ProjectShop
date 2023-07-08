1c4de04cf0971684edeb5735fdcc41c31a8af962
Daniel <derrysantaclos@gmail.com>
2023-07-08 22:17:39 +0100

    generate code and confirm agreement checked

--- src/components/SignInBody/SignInBody.jsx
+++ src/components/SignInBody/SignInBody.jsx
@@ -1,9 +1,20 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './SignInBody.css'
 
 const SignInBody = (props) => {
   const emailRef =useRef(null);
+  const [code, setCode]= useState(0);
+  const [isAgreed, setIsAgreed]=useState(false)
+  const generateCode = ()=>{
+    setCode(Math.floor(Math.random()*1000000))};
   const [userEmail, setUserEmail] = useState("")
+  const validateEmail =()=>{
+    return(/\S+@\S+\.\S+/.test(userEmail))
+  }
+  const handleChange =(event)=>{
+    setIsAgreed(event.target.checked)
+  }
+  useEffect(generateCode,[])
   return (
     <div className="wrapper">
         <div >
@@ -15,12 +26,20 @@ const SignInBody = (props) => {
             <div className="policy-text">
                 <p>I agree to <b>Th3F4ctory's</b>  <span className='inline-link'>Privacy Policy</span>  and  <span className='inline-link'>Terms of Use</span></p>
             
-                <input className='policy-input' type="checkbox" />
+                <input className='policy-input' onChange={handleChange} type="checkbox" />
             
             </div>
            <div className="btn-wrapper">
-           <button className='action-btn' onClick={()=>{
-            console.log(emailRef.current.value)
+           <button className='action-btn' disabled= {!isAgreed} onClick={()=>{
+            if (validateEmail()){
+              generateCode()
+              console.log(code)
+              
+            }
+            else{
+              alert("fail")
+            }
+            // console.log(emailRef.current.value)
 
             props.onSendCodeBtn(userEmail)}}>Send Code</button>
           
